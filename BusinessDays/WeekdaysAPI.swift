e781ee5979ee06fbbab29f5907e8eb2abbee4f0a
acicartagena <acicartagena@gmail.com>
2020-04-11 22:39:37 +1000

    Added simple weekdaysAPI

--- BusinessDays/WeekdaysAPI.swift
+++ BusinessDays/WeekdaysAPI.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+enum WeekdaysAPIError: Error {
+    case invalidDate
+}
+
+class WeekdaysAPI {
+    let calendar = Calendar(identifier: .gregorian)
+
+    func weekdayCounts(from fromDate: Date, to toDate: Date, completion: (Result<Int, WeekdaysAPIError>) -> Void) {
+
+        var weekdayCount = 0
+        guard let adjustedDate = calendar.date(byAdding: .day, value: 1, to: fromDate) else {
+            completion(.failure(.invalidDate))
+            return
+        }
+
+        var date = adjustedDate
+        while calendar.compare(date, to: toDate, toGranularity: .day) != .orderedSame {
+            if !calendar.isDateInWeekend(date) {
+                weekdayCount += 1
+            }
+
+            if let newDate = calendar.date(byAdding: .day, value: 1, to: date) {
+                date = newDate
+            } else {
+                completion(.failure(.invalidDate))
+                return
+            }
+        }
+        completion(.success(weekdayCount))
+    }
+}
