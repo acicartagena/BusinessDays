3180ff95887889739853d2f0a79c4202d933013b
acicartagena <acicartagena@gmail.com>
2020-04-13 21:08:20 +1000

    updated HolidaysAPITests

--- BusinessDays/API/HolidaysAPI.swift
+++ BusinessDays/API/HolidaysAPI.swift
@@ -38,27 +38,30 @@ class LoopDaysHolidaysEngine : HolidaysAPI {
                 guard let date = holidayDate.date(calendar: calendar, year: year),
                     !calendar.isDateInWeekend(date) else { continue }
                 holidayDates.append(date)
-            case let .sameDateOrNextWeekday(holidayDate):
-                guard let date = holidayDate.date(calendar: calendar, year: year) else { continue }
-                if !calendar.isDateInWeekend(date) {
-                    holidayDates.append(date)
-                } else {
-                    var nextDate: Date = date
 
-                    while calendar.isDateInWeekend(nextDate) ||
-                        holidayDates.contains { calendar.isDate($0, inSameDayAs: nextDate) } {
-                        guard let next = calendar.date(byAdding: .day, value: 1, to: nextDate) else { break }
-                        nextDate = next
-                    }
-
-                    if calendar.compare(date, to: nextDate, toGranularity: .day) == .orderedAscending {
-                        holidayDates.append(date)
-                    }
-                }
             case let .dayInAMonth(holidayDayInMonth):
                 guard let date = holidayDayInMonth.date(calendar: calendar, year: year),
                     !calendar.isDateInWeekend(date) else { continue }
                 holidayDates.append(date)
+
+            case let .sameDateOrNextWeekday(holidayDate):
+                guard let date = holidayDate.date(calendar: calendar, year: year) else { continue }
+                guard calendar.isDateInWeekend(date) else {
+                    holidayDates.append(date)
+                    continue
+                }
+
+                var nextDate: Date = date
+
+                while calendar.isDateInWeekend(nextDate) ||
+                    holidayDates.contains { calendar.isDate($0, inSameDayAs: nextDate) } {
+                    guard let next = calendar.date(byAdding: .day, value: 1, to: nextDate) else { break }
+                    nextDate = next
+                }
+
+                if calendar.compare(date, to: nextDate, toGranularity: .day) == .orderedAscending {
+                    holidayDates.append(nextDate)
+                }
             }
         }
         completion(.success(holidayDates))
--- BusinessDays/API/HolidaysAPITests.swift
+++ BusinessDays/API/HolidaysAPITests.swift
@@ -18,19 +18,41 @@ class HolidaysAPITests: XCTestCase {
     }
 
     func testAllWeekdayHolidaysForYear() {
+        let year = 2018
         let expectedDates: [Date] = [
-            DateComponents(calendar: calendar, year: 2018, month: 1, day: 1).date!,
-            DateComponents(calendar: calendar, year: 2018, month: 1, day: 26).date!,
-            DateComponents(calendar: calendar, year: 2018, month: 4, day: 25).date!,
-            DateComponents(calendar: calendar, year: 2018, month: 6, day: 11).date!,
-            DateComponents(calendar: calendar, year: 2018, month: 10, day: 1).date!,
-            DateComponents(calendar: calendar, year: 2018, month: 12, day: 25).date!,
-            DateComponents(calendar: calendar, year: 2018, month: 12, day: 26).date!,
+            DateComponents(calendar: calendar, year: year, month: 1, day: 1).date!,
+            DateComponents(calendar: calendar, year: year, month: 1, day: 26).date!,
+            DateComponents(calendar: calendar, year: year, month: 4, day: 25).date!,
+            DateComponents(calendar: calendar, year: year, month: 6, day: 11).date!,
+            DateComponents(calendar: calendar, year: year, month: 10, day: 1).date!,
+            DateComponents(calendar: calendar, year: year, month: 12, day: 25).date!,
+            DateComponents(calendar: calendar, year: year, month: 12, day: 26).date!,
         ]
 
-        subject.weekdayHolidays(for: 2018) { result in
+        subject.weekdayHolidays(for: year) { result in
             switch result {
             case let .success(dates): XCTAssertEqual(dates, expectedDates)
+            case .failure: XCTFail("Expecting array of dates")
+            }
+        }
+    }
+
+    func testWeekdayHolidaysNoAnzacBoxingDayOnMonday() {
+        let year = 2015
+        let expectedDates: [Date] = [
+            DateComponents(calendar: calendar, year: year, month: 1, day: 1).date!,
+            DateComponents(calendar: calendar, year: year, month: 1, day: 26).date!,
+            DateComponents(calendar: calendar, year: year, month: 6, day: 8).date!,
+            DateComponents(calendar: calendar, year: year, month: 10, day: 5).date!,
+            DateComponents(calendar: calendar, year: year, month: 12, day: 25).date!,
+            DateComponents(calendar: calendar, year: year, month: 12, day: 28).date!,
+        ]
+
+        subject.weekdayHolidays(for: year) { result in
+            switch result {
+            case let .success(dates):
+                print(dates)
+                XCTAssertEqual(dates, expectedDates)
             case .failure: XCTFail("Expecting array of dates")
             }
         }
